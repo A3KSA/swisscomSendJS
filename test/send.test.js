d9607394759de755c79069d85806b2d3729dab72
ch-zacmo <zacmonnet@gmail.com>
2024-01-10 15:16:24 +0100

    added test script

---
+++ test/send.test.js
@@ -0,0 +1,36 @@
+const nock = require('nock');
+const Send = require('../src/send');
+
+describe('getDeliveryStatus', () => {
+    let send;
+    let messageId;
+    let callback;
+
+    beforeEach(() => {
+        send = new Send();
+        messageId = '12345';
+        callback = jest.fn();
+    });
+
+    it('should call the callback with the response when the status is 200', async () => {
+        const response = { status: 'delivered' };
+
+        nock('https://api.swisscom.com')
+            .get(`/messaging/sms/${messageId}`)
+            .reply(200, response);
+
+        await send.getDeliveryStatus(messageId, callback);
+
+        expect(callback).toHaveBeenCalledWith(response);
+    });
+
+    it('should throw an error when the status is not 200', async () => {
+        const response = { error: 'Something went wrong' };
+
+        nock('https://api.swisscom.com')
+            .get(`/messaging/sms/${messageId}`)
+            .reply(400, response);
+
+        await expect(send.getDeliveryStatus(messageId, callback)).rejects.toThrow('Something went wrong with the Swisscom get delivery status API !');
+    });
+});
